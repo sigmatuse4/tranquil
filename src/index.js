9e903f7f8dd70ff8b579b593ac8e7117868af5fd
Ivan <zeroexabyte567@gmail.com>
2021-02-02 10:40:04 +0800

    I dunno

---
+++ src/index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const Client = require('../structures/Client');
+const client = new Client({
+    clientOptions: {
+        shardCount: 2,
+        disableMentions: 'everyone'
+    },
+    token: process.env.TOKEN
+});
+
+client.on('ready', () => console.log(`${client.user.tag} is now online!`));
+client.on('message', async (message) => {
+    if (message.author.bot && !message.guild.available) return undefined;
+
+    const args = message.content.slice(client.config.prefix.length).trim().split(/ +/g);
+    const cmd = args.shift().toLowerCase();
+
+    console.log(cmd);
+});
+
+client.login(client.config.token);
---
+++ structures/Client.js
@@ -0,0 +1,21 @@
+// referenced from https://github.com/iCrawl/discord-music-bot/
+// referenced from MenuDocs
+
+const { Client, Collection } = require('discord.js');
+const Music = require('./Music');
+
+module.exports = class extends Client {
+    constructor(config) {
+
+        const clientOptions = 
+            typeof config.clientOptions === 'object' ? config.clientOptions : {
+                disableMentions: 'everyone'
+            };
+        super(clientOptions);
+
+        this.commands = new Collection();
+        this.aliases = new Collection();
+        this.music = new Music(this);
+
+    }
+}
